bd07932d81298c4dc0c5e8a14353eb884c027772
w3manik <73774596+w3manik@users.noreply.github.com>
2022-06-13 09:08:21 +0600

    lilac
    test

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.testi_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow:'.first',
+        nextArrow:'.secoun',
+        arrows:true,
+        autoplaySpeed: 2000,
+      });
+})
